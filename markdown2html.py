26230cba8934f0829fe8f9c76fca6913c0d3c6e6
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-12-12 10:58:16 +0000

    improve script by parsing Headings Markdown syntax for generating HTML

--- markdown2html.py
+++ markdown2html.py
@@ -14,16 +14,33 @@ def main():
             "Missing <filename>" to STDERR: If the Markdown file doesn’t exist
     """
 
+    # Checking if number of CLI arguments is wright
     if len(sys.argv) != 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
         sys.exit(1)
 
     md_file = sys.argv[1]
+    html_file = sys.argv[2]
+
+    # Checking if markdown file exists
     if not os.path.isfile(md_file):
         print(f"Missing {md_file}", file=sys.stderr)
         sys.exit(1)
 
+    # Parsing the markdown file and writing to a new html file
+    with open(md_file, "r", encoding='UTF-8') as markdown:
+        with open(html_file, "w", encoding='UTF-8') as html:
+            while True:
+                line = markdown.readline()
+                if not line:
+                    break
+                if line.startswith("#"):
+                    count = line.count("#")
+                    text = line.rsplit('# ', 1)[1].strip()
+                    html.write(f"<h{count}>{text}</h{count}>\n")
+
+    # Exits after sucessful program execution
     sys.exit()
 
 
