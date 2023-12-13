c3f3a77f84f4aa2b0f84465d2d605015fb8b380b
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-12-13 14:18:39 +0000

    add algorithm to encode part of string avec md5 and remove c characters

--- markdown2html.py
+++ markdown2html.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A script converting Markdown markup language files into HTML files"""
 
+import hashlib
 import os
 import re
 import sys
@@ -16,6 +17,18 @@ def replace_em(match):
     """A function that returns a formatted em string html"""
     em_text = match.group(1)
     return f'<em>{em_text}</em>'
+
+
+def encode_md5(match):
+    """A function that returns a MD5 encoded string"""
+    text_to_encode = match.group(1)
+    return hashlib.md5(text_to_encode.encode()).hexdigest()
+
+
+def remove_c(match):
+    """A function that returns a string without "c" characters"""
+    text_with_c = match.group(1)
+    return text_with_c.replace('c', '').replace('C', '')
 
 
 def main():
@@ -44,11 +57,17 @@ def main():
     # Parsing the markdown file and writing html to a new html file
     with open(md_file, "r", encoding='UTF-8') as markdown:
         with open(html_file, "w", encoding='UTF-8') as html:
+
+            # HTML tags
             ol_open = False
             ul_open = False
             p_open = False
+
+            # Special patterns
             pattern_bold = re.compile(r'\*\*(.*?)\*\*')
             pattern_em = re.compile(r'__(.*?)__')
+            pattern_md5 = re.compile(r'\[\[(.*?)\]\]')
+            pattern_c = re.compile(r'\(\((.*?)\)\)')
 
             # The loop to parse the file
             while True:
@@ -76,9 +95,12 @@ def main():
                 if not line:
                     break
 
-                # Evaluation em and bold contents in the line
+                # Evaluation em, bold, md5, strip "c"
+                # chars contents in the line
                 line = re.sub(pattern_bold, replace_bold, line)
                 line = re.sub(pattern_em, replace_em, line)
+                line = re.sub(pattern_md5, encode_md5, line)
+                line = re.sub(pattern_c, remove_c, line)
 
                 # Generates Headings
                 if line.startswith("#"):
